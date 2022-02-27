ebf117e89e3349d639e3c77938551de6e31b3bbb
Surajit Karuri <surajitkaruri@gmail.com>
2022-02-28 00:27:07 +0530

    removed int check for rating

--- validations/reviewValidators.js
+++ validations/reviewValidators.js
@@ -1,14 +1,6 @@
 const { body, param } = require('express-validator');
 
 exports.addReviewValitor = [
-  body('rating')
-    .isInt()
-    .custom((value) => {
-      if (![1, 2, 3, 4, 5].includes(value)) {
-        throw new Error('Invalid rating');
-      }
-      return true;
-    }),
   body('comments')
     .optional()
     .isString()
